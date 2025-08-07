23c38f800dd6fe9a360fdcc47accd50ae148e41d
Dennis Rocha <dennis0201@gmail.com>
2025-08-07 20:07:40 -0300

    Aula 4 commit 1

--- meuprimeiroprojeto/primeiroapp/views.py
+++ meuprimeiroprojeto/primeiroapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'primeiroapp/pages/home.html', context={'lista_num': [1,2,3,4,5,6]})
 
 def sobre(request):
-    return render(request, 'sobre.html')
+    return render(request, 'primeiroapp/pages/sobre.html')
