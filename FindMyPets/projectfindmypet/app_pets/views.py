82f4fd94bd1a0d9664aa2ef4931b3f80d1745518
Junior Kabelo <jrkabelo@gmail.com>
2025-08-14 02:38:04 -0300

    Endpoints de pets e tutores adicionados

--- FindMyPets/projectfindmypet/app_pets/views.py
+++ FindMyPets/projectfindmypet/app_pets/views.py
@@ -1,6 +1,15 @@
+from rest_framework import viewsets
+from .models import Pet, Tutor
+from .serializers import PetSerializer, TutorSerializer
 from django.shortcuts import render
-
 
 def home(request):
     return render(request, 'app_pets/pages/home.html')
 
+class PetViewSet(viewsets.ModelViewSet):
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
+
+class TutorViewSet(viewsets.ModelViewSet):
+    queryset = Tutor.objects.all()
+    serializer_class = TutorSerializer
