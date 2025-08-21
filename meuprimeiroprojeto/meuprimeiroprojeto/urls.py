8b849b79ac27c094157634376d36914fe675b467
GuiHackingMC <guilhermemoretto59@gmail.com>
2025-08-21 19:50:02 -0300

    Fixes-P1

--- meuprimeiroprojeto/meuprimeiroprojeto/urls.py
+++ meuprimeiroprojeto/meuprimeiroprojeto/urls.py
@@ -21,5 +21,6 @@ from primeiroapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('sobre/', views.sobre)
+    path('sobre/', views.sobre),
+    path('teste/', views.teste)
 ]
--- meuprimeiroprojeto/primeiroapp/views.py
+++ meuprimeiroprojeto/primeiroapp/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+lista_produtos = [
+    {
+        'id': 1,
+        'nome': 'Camisa',
+        'preco': 100
+    }
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'primeiroapp/pages/home.html', context={'lista_num': [1,2,3,4,5,6]})
 
 def sobre(request):
     return render(request, 'primeiroapp/pages/sobre.html')
+
+def teste(request):
+    return render(request, 'primeiroapp/pages/teste.html'), {'lista_produtos': lista_produtos}
