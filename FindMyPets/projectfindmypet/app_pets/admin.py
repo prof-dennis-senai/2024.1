2c69fb51dc3ba399ad6086385e7b7a1f4feb21cb
Dennis Rocha <dennis0201@gmail.com>
2025-08-21 22:04:13 -0300

    feat: Adicionado a primeira tabela Pet

--- FindMyPets/projectfindmypet/app_pets/admin.py
+++ FindMyPets/projectfindmypet/app_pets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import PetModels
+
 
 # Register your models here.
+@admin.register(PetModels)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'raca', 'especie', 'sexo', 'porte', 'cor', 'status')
+    search_fields = ('nome', 'raca', 'especie', 'status')
+    list_filter = ('especie', 'sexo', 'porte', 'cor', 'status')
--- FindMyPets/projectfindmypet/app_pets/models.py
+++ FindMyPets/projectfindmypet/app_pets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class PetModels(models.Model):
+    nome = models.CharField(max_length=100)
+    data_nascimento = models.DateField()
+    especie = models.CharField(max_length=100)
+    raca = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=100)
+    porte = models.CharField(max_length=100)
+    cor = models.CharField(max_length=100)
+    descricao = models.TextField()
+    microchip_codigo = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'PetModels(nome ="{self.nome}", raca="{self.raca}")'
